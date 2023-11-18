169787a2abf6b99ba7c939c7dad0c734cb0b9595
lellerena <lellerena@uninorte.edu.co>
2023-11-18 18:40:45 -0500

    modified login to use cookies to create sessions

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -1,4 +1,12 @@
 import { usuarioHandler } from 'db/handler.js'
+import bcrypt from 'bcrypt'
+import Cookies from 'js-cookie'
+import { v4 as uuid } from 'uuid'
+
+const cookieOptions = {
+    maxAge: 1000 * 60 * 60 * 60 * 24, // 7 days
+    httpOnly: true
+}
 
 export const login = async (req, res) => {
     const { user, pass } = req.body
@@ -19,12 +27,37 @@ export const login = async (req, res) => {
         return res.status(400).json({ error: 'Usuario no encontrado' })
     }
 
-    if (usuario.contrasena !== pass) {
-        console.log(usuario.contrasena, pass)
+    if ((await checkPassword(pass, usuario.contrasena)) === false) {
+        // console.log(usuario.contrasena, pass)
         return res.status(400).json({ error: 'Contraseña incorrecta' })
     }
 
+    // Create session cookie
+    const sessionCookie = createSessionCookie(user)
+
+    // Set the session cookie in the response
+    res.cookie('session', sessionCookie, cookieOptions)
+
     res.status(200).json({ message: 'Usuario encontrado' })
+}
+
+export const renewSession = async (req, res) => {
+    const sessionCookie = req.cookies.session
+
+    if (!sessionCookie) {
+        return res.status(400).json({ error: 'Sesión no encontrada' })
+    }
+
+    // extract data from de cookie using Cookie
+    const { user, ...rest } = sessionCookie
+    console.log(rest)
+    // Create session cookie
+    const newSessionCookie = createSessionCookie(user)
+
+    // Set the session cookie in the response
+    res.cookie('session', newSessionCookie, cookieOptions)
+
+    res.status(200).json({ message: 'Sesión renovada' })
 }
 
 function validateUser(user) {
@@ -33,4 +66,32 @@ function validateUser(user) {
 
 function validatePassword(pass) {
     return true // Aquí debes implementar la lógica de validación de la contraseña
+}
+
+async function checkPassword(pass, hashedPass) {
+    const compare = await bcrypt.compare(pass, hashedPass)
+    return compare
+}
+
+function createSessionCookie(user) {
+    // Generate a unique session ID
+    const sessionId = generateSessionId()
+
+    const sesion = {
+        id: sessionId,
+        user
+    }
+
+    // Set the session ID in the cookie
+    Cookies.set('session', sesion, { expires: 7 })
+
+    return sesion
+}
+
+function generateSessionId() {
+    // Generate a random session ID using a library or algorithm of your choice
+    // For example, you can use the uuid library to generate a UUID
+    const sessionId = uuid()
+
+    return sessionId
 }
