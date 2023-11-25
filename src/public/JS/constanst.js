e95baf72d0de5295561905bfb4b8ca9d6a4d23a6
lellerena <lellerena@uninorte.edu.co>
2023-11-25 11:55:40 -0500

    constantes y animaciones agreagadas

--- src/public/JS/constanst.js
+++ src/public/JS/constanst.js
@@ -2,3 +2,6 @@
 
 const backend_url = 'http://localhost:3000'
 const login_url = `${backend_url}/api/auth/login`
+const register_url = `${backend_url}/api/auth/register`
+const programs_url = `${backend_url}/api/programa`
+const areas_url = `${programs_url}/area`
